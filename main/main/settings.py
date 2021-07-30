3cbd8b7208a3549ceebed86211dee3b54a186f81
Dan <dan@Danniels-MacBook-Air.local>
2021-07-30 16:56:08 -0700

    created base models

--- main/main/settings.py
+++ main/main/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'view',
+    'view.apps.ViewConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- main/view/models.py
+++ main/view/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class project(models.Model):
+    project_name = models.CharField(max_length = 64)
+    project_desc = models.TextField(max_length=500, blank=True)
+    project_id = models.BigAutoField(primary_key=True)
+    project_start_date = models.DateField(auto_now_add=True)
+    project_goal_date = models.DateField(blank=True)
+    def __str__(self):
+        return self.project_name
+
+
