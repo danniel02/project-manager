ddc31be23b87243f379b027c9917739c38feaf61
Dan <dan@Danniels-MacBook-Air.local>
2021-07-30 18:22:12 -0700

    added tasks, users model + format files

--- main/view/models.py
+++ main/view/models.py
@@ -10,4 +10,22 @@ class project(models.Model):
     def __str__(self):
         return self.project_name
 
+class tasks(models.Model):
+    task_id = models.BigAutoField(primary_key=True)
+    task_name = models.CharField(max_length=64)
+    task_desc = models.TextField(max_length=500)
+    task_start_date = models.DateField(auto_now_add=True)
+    task_end_date = models.DateField
+    def __str__(self):
+        return self.task_name
 
+class users(models.Model):
+    users_id = models.BigAutoField(primary_key=True)
+    users_first = models.CharField(max_length=64)
+    users_last = models.CharField(max_length=64)
+    users_username = models.CharField(max_length=64)
+    users_password = models.CharField(max_length=64)
+    users_bio = models.TextField(max_length=500)
+    users_email = models.CharField(max_length=64)
+    def __str__(self):
+        return self.users_username
--- main/view/views.py
+++ main/view/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "view/index.html")
+
+def project(request, project_id):
+    #todo
+    return HttpResponse(f"WIP: returns the project page of project id: {project_id}")
